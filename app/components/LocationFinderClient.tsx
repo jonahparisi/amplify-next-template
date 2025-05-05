7a2b77d8fc4ea4f131062a5d60f5656664f77f7e
jonahparisi <youngmiamiheatfan@gmail.com>
2025-05-04 23:04:34 -0500

    Finished project 4

--- app/components/LocationFinderClient.tsx
+++ app/components/LocationFinderClient.tsx
@@ -3,7 +3,12 @@
 import { useEffect, useState } from "react";
 
 export default function LocationFinderServer() {
-  const [locationInfo, setLocationInfo] = useState({ City: "N/A" });
+  const [locationInfo, setLocationInfo] = useState({
+    City: "N/A",
+    Latitude: "N/A",
+    Longitude: "N/A",
+  });
+  const [weatherInfo, setWeatherInfo] = useState({ temp2m: "N/A" });
 
   const getLocationInfo = async () => {
     const response = await fetch("https://apip.cc/json");
@@ -12,12 +17,32 @@ export default function LocationFinderServer() {
     setLocationInfo(locationData);
   };
 
+  const getWeatherInfo = async () => {
+    const response = await fetch(
+      "https://www.7timer.info/bin/astro.php?lon=" +
+        locationInfo.Longitude +
+        "&lat=" +
+        locationInfo.Latitude +
+        "&ac=0&unit=metric&output=json&tzshift=0"
+    );
+    const weatherData = await response.json();
+    setWeatherInfo(weatherData.dataseries[0]);
+  };
+
   useEffect(() => {
     getLocationInfo();
   }, []);
+
+  useEffect(() => {
+    if (locationInfo.Latitude !== "N/A" && locationInfo.Longitude !== "N/A") {
+      getWeatherInfo();
+    }
+  }, [locationInfo]);
   return (
     <>
+      <small>client component</small>
       <h1>Hello from {locationInfo.City} - Client</h1>
+      <h3>Its currently {weatherInfo.temp2m} degrees celcius</h3>
     </>
   );
 }
--- app/components/LocationFinderServer.tsx
+++ app/components/LocationFinderServer.tsx
@@ -4,9 +4,21 @@ export default async function LocationFinderServer() {
   console.log(locationData);
   const locationInfo = locationData;
 
+  const response2 = await fetch(
+    "https://www.7timer.info/bin/astro.php?lon=" +
+      locationInfo.Longitude +
+      "&lat=" +
+      locationInfo.Latitude +
+      "&ac=0&unit=metric&output=json&tzshift=0"
+  );
+  const weatherData = await response2.json();
+  const weatherInfo = weatherData.dataseries[0];
+
   return (
     <>
-      <h1>Hello from {locationInfo.City} - Server</h1>
+      <small>server component</small>
+      <h1>Hello from {locationInfo.City}</h1>
+      <h3>Its currently {weatherInfo.temp2m} degrees celcius</h3>
     </>
   );
 }
